772740d0372d29237e0992ea8b0665d6a244594d
tictrac1985 <tictrac1985@users.noreply.github.com>
2026-08-18 15:31:10 +0300

    Deploy static export of Almobadarat website

---
+++ _next/static/T0kYqJ7lA54FVyqcT_GIW/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fabout","\u002F[locale]\u002Fcontact","\u002F[locale]\u002Fknowledge","\u002F[locale]\u002Fprojects","\u002F[locale]\u002Fservices","\u002F[locale]\u002Fservices\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
